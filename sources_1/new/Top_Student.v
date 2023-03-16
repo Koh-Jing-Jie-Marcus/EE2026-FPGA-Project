2941295b3bbb7f1dc31c32961040c84e02f70e81
TJ-Hoo <88079327+TJ-Hoo@users.noreply.github.com>
2023-03-16 21:34:18 +0800

    Add files via upload

--- sources_1/new/Top_Student.v
+++ sources_1/new/Top_Student.v
@@ -10,60 +10,47 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module Top_Student (
-    // Delete this comment and include Basys3 inputs and outputs here
     input clock,
-    input sw4,
-    //For OLED display
-    inout PS2Clk, PS2Data,
-    output led15, led14, led13,
-    output [7:0] JC
-    );
-    //mouse instantiation
-    reg rst = 0;
-    wire[11:0] mouse_xpos, mouse_ypos;
-    wire[3:0] mouse_zpos;
-    wire mouse_left_click, mouse_middle_click, mouse_right_click, mouse_new_event;
-    MouseCtl mouse(
-    .clk(clock), .rst(rst), 
-    .xpos(mouse_xpos), .ypos(mouse_ypos), .zpos(mouse_zpos), 
-    .left(mouse_left_click), .middle(mouse_middle_click), .right(mouse_right_click), .new_event(mouse_new_event),
-    .value(0), .setx(0), .sety(0), .setmax_x(0), .setmax_y(0),
-    .ps2_clk(PS2Clk), .ps2_data(PS2Data)
+    input [15:0] pixel_data,
+    input [15:0] sw,
+    output [7:0] Jc
     );
     
-    //OLED Instantiation
-    wire clk6p25m;
-    clk_div slow_clk6p25(clock,7,clk6p25m);
-    wire [15:0] oled_data; 
-    wire frame_begin;
+    wire clk_6p25mhz;
+    my_clock_divider clk6p25mHz(clock,7, clk_6p25mhz);
+    
+    wire [15:0] oled_data;
+    wire frame_begin, sending_pixel, sample_pixel;
     wire [12:0] pixel_index;
-    wire sending_pixels;
-    wire sample_pixel;
-    Oled_Display oled(
-      .clk(clk6p25m), .reset(rst), .frame_begin(frame_begin), .sending_pixels(sending_pixels),
-      .sample_pixel(sample_pixel), .pixel_index(pixel_index), .pixel_data(oled_data), 
-      .cs(JC[0]), .sdin(JC[1]), .sclk(JC[3]), .d_cn(JC[4]), .resn(JC[5]), .vccen(JC[6]), .pmoden(JC[7])
-    );
     
-    //pre-processing
-    wire [6:0] oled_x;
-    wire [6:0] oled_y;
-    oled_x_y_coord xy(pixel_index, oled_x, oled_y);
-    wire [6:0] mouse_x_scale;
-    wire [6:0] mouse_y_scale;
-    mouse_xy_scale xy_scale(mouse_xpos, mouse_ypos, mouse_x_scale, mouse_y_scale);
-    wire [15:0] c_indiv_oled_data;
-    stu_C_indiv_task c_indiv_task(clock, mouse_middle_click, oled_x, oled_y, mouse_x_scale, mouse_y_scale, c_indiv_oled_data);
-    wire [15:0] ftw_oled_data;
-    find_the_white ftw_game(clock, mouse_left_click, oled_x, oled_y, ftw_oled_data);
-    wire is_ftw;
-    assign is_ftw = sw4;
+    Oled_Display oled (
+            .clk(clk_6p25mhz),
+            .reset(0),
+            .frame_begin(frame_begin),
+            .sending_pixels(sending_pixel),
+            .sample_pixel(sample_pixel),
+            .pixel_index(pixel_index),
+            .pixel_data(oled_data),
+            .cs(Jc[0]),
+            .sdin(Jc[1]),
+            .sclk(Jc[3]),
+            .d_cn(Jc[4]),
+            .resn(Jc[5]),
+            .vccen(Jc[6]),
+            .pmoden(Jc[7])
+            );
+            
+    wire [6:0] x;
+    wire [6:0] y;
+    oled_x_y_coord xy(pixel_index, x, y);
     
-    //assignment
-    assign led15 = (mouse_left_click)? 1 : 0;
-    assign led14 = (mouse_middle_click)? 1 : 0;
-    assign led13 = (mouse_new_event)? 1 : 0;
-    assign oled_data = is_ftw ? ftw_oled_data : c_indiv_oled_data;
+    wire [15:0] d_indiv_oled_data;
+    //stu_D_indiv_task d_indiv_task(clock, x, y, sw, d_indiv_oled_data);
+    //assign oled_data = d_indiv_oled_data;     
+    
+    wire[15:0] group_task_oled_data;
+    group_task task_group(clock, x, y, sw, group_task_oled_data);   
+    assign oled_data = group_task_oled_data;
+    
 endmodule
---
+++ sources_1/new/group_task.v
@@ -0,0 +1,84 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 16.03.2023 20:52:31
+// Design Name: 
+// Module Name: group_task
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module group_task(
+    input clock,
+    input [6:0] x, y,
+    input [15:0] sw,
+    output reg [15:0] oled_data
+    );
+    
+    always @ (posedge clock) begin 
+        if ((x >= 57) && (x <= 59) && (y >= 1) && (y <= 59)) 
+             begin
+                  oled_data <= (sw[0]) ? 16'b00000_000000_00000 : 16'b00000_111111_00000;
+             end
+        else if ((y >= 57) && (y <= 59) && (x >= 1) && (x <= 59))
+             begin
+                  oled_data <= (sw[0]) ? 16'b00000_000000_00000 : 16'b00000_111111_00000;
+             end
+        else if ((x >= 16) && (x <= 42) && (y == 11))  
+             begin
+                  oled_data <= 16'b11111_111111_11111;
+             end
+        else if ((x >= 16) && (x <= 42) && (y == 13))  
+             begin
+                  oled_data <= 16'b11111_111111_11111;
+             end
+        else if ((x >= 16) && (x <= 42) && (y == 29))  
+              begin
+                  oled_data <= 16'b11111_111111_11111;
+              end
+        else if ((x >= 16) && (x <= 42) && (y == 31))  
+              begin
+                  oled_data <= 16'b11111_111111_11111;
+              end
+        else if ((x >= 16) && (x <= 42) && (y == 46))  
+              begin
+                  oled_data <= 16'b11111_111111_11111;
+              end
+        else if ((x >= 16) && (x <= 42) && (y == 48))  
+              begin
+                  oled_data <= 16'b11111_111111_11111;
+              end
+        else if ((x == 16) && (y >= 11) && (y <= 48))  
+              begin
+                  oled_data <= 16'b11111_111111_11111;
+              end
+        else if ((x == 18) && (y >= 11) && (y <= 48))  
+              begin
+                   oled_data <= 16'b11111_111111_11111;
+              end
+        else if ((x == 40) && (y >= 11) && (y <= 48))  
+              begin
+                   oled_data <= 16'b11111_111111_11111;
+              end
+        else if ((x == 42) && (y >= 11) && (y <= 48))  
+              begin
+                   oled_data <= 16'b11111_111111_11111;
+              end
+        else
+              begin 
+                   oled_data <= 16'b00000_000000_00000;
+              end
+        end
+endmodule
