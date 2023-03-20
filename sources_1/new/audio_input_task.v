691cc490ac7735508cabeadbae9bfe8d9c16713a
Koh Ngiap Hin <64647635+kohnh@users.noreply.github.com>
2023-03-20 12:46:46 +0800

    team integration

--- sources_1/new/audio_input_task.v
+++ sources_1/new/audio_input_task.v
@@ -1,17 +1,17 @@
 module audio_input_task (
+    input clock_20khz,
     output reg [8:0] led = 9'b000000000,
-    output reg [1:0] an0 = 1,
+    output reg an0 = 1,
     output reg [3:0] audio_input_number
     );
 
     reg [31:0] count = 32'b00000000000000000000000000000000;
     reg [11:0] peak_value = 12'b000000000000;
     reg [11:0] current_value = 12'b000000000000;
-    wire clock_20khz;
     wire [11:0] MIC_in;
     wire [3:0] volume_level;
     wire [8:0] LED; 
-    wire [1:0] AN0;
+    wire AN0;
     wire [6:0] SEG;
     Volume_level unit_level(peak_value, volume_level);
     Update_display unit_display(volume_level, LED, AN0, SEG);
